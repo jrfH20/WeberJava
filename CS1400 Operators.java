d73a4e67780d8492aade265dcd42b5c01b9aa0da
jrfH20 <eferg_76@yahoo.com>
2016-04-29 19:21:37 -0600

    1st
    1st

---
+++ CS1400 Operators.java
@@ -0,0 +1,67 @@
+
+public class DemoApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x;
+		double d;
+		float f;
+		String s;
+		long l;
+		char c;
+		
+		x = 5;
+		int y = 8;
+		y = 10;
+		
+		x = 10;
+		x = 5;
+	
+		
+		d = 30.3;
+		
+		s = "Matt";
+		
+		d = d * 6; 
+		
+		d *= 6;
+		
+		d += 3;
+		
+		d /= 24.3;
+		
+		d -= 3;
+		
+		d = d / 5.4;
+		
+		x = x + y;
+		
+		x = x - y;
+		
+		x = x + 1;
+		
+		x++;
+		
+		y = y - 1;
+		
+		y--;
+		System.out.println(x);	
+		
+		x = x % 2;
+		
+		
+		
+		
+		
+		System.out.println(x);
+		
+		
+
+		
+	}
+
+}
